1e71d6182a6e0077f8a6337604e19918f47785d0
Leo Chang-Liao <leo.changliao@gmail.com>
2019-08-21 18:14:06 -0400

    resume - implement static content

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss']
 })
-export class ResumeComponent implements OnInit {
+export class ResumeComponent implements OnInit, OnDestroy {
 
+  isReady:boolean = false;
+  private stopTimer:any = null;
   constructor() { }
 
   ngOnInit() {
+    /* initiate skill bar if no mouse movement entering the page */
+    this.stopTimer = setTimeout(()=>{
+      this.setSkillBar();
+    },2000);
   }
 
+  ngOnDestroy() {
+    clearTimeout(this.stopTimer);
+  }
+
+  setSkillBar() {
+    this.isReady = true;
+  }
 }
