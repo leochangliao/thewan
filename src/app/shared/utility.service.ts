b79639c3016ea3255caa863cf0be567c5c89eee6
Leo Chang-Liao <leo.changliao@gmail.com>
2019-08-22 17:41:38 -0400

    implement cache on http GET request

--- src/app/shared/utility.service.ts
+++ src/app/shared/utility.service.ts
@@ -1,14 +1,26 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, of} from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { AppSettings } from '../app.setting';
-import { Observable } from 'rxjs';
 
 @Injectable({providedIn: 'root',})
 
 export class UtilityService {
     private today : Date = new Date();
+    private cacheData:any = {
+        portfolio: {
+            observable:null,
+            data:null
+        },
+        resume: {
+            observable:null,
+            data:null
+        }
+    }
+
     constructor (private settings:AppSettings,
                  private http:HttpClient) {}
 
@@ -16,11 +28,32 @@ export class UtilityService {
         return this.settings.imagePath + 'wallpaper/wallpaper-month-' + (this.today.getMonth() + 1) + '.jpg';
     }
 
+    /**
+     * return cache data from HTTP GET request if aplicable
+     * @param handler get request data object
+     * @param url get request url
+     */
+    private getHttpRequest(handler:any, url:string):Observable<any> {
+        if(handler.data){
+            return of(handler.data);
+        } else if(handler.observable){
+            return handler.observable;
+        } else {
+            handler.observable = this.http.get<any>(url)
+                .pipe(map(response =>  {
+                    handler.observable = null;
+                    handler.data = response;
+                    return response;
+                }));
+            return handler.observable;
+        }
+    }
+
     getPortfolio():Observable<any> {
-        return this.http.get(this.settings.apiUrls.portfolio);
+        return this.getHttpRequest(this.cacheData.portfolio, this.settings.apiUrls.portfolio);
     }
 
     getResume():Observable<any> {
-        return this.http.get(this.settings.apiUrls.resume);
+        return this.getHttpRequest(this.cacheData.resume, this.settings.apiUrls.resume);
     }
 }
