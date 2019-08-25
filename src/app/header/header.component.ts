defd8dfeb9375b7c7ef3fcf2d429eb8d56536408
Leo Chang-Liao <leo.changliao@gmail.com>
2019-08-25 13:58:25 -0400

    implement fly out menu when menu is invisible on top

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+
+export class HeaderComponent implements OnInit, OnDestroy {
   toggleMenu:boolean=false;
+  renderFixedMenu:boolean=false;
   constructor() { }
 
   ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true);
   }
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
+  }
+
+  scroll = (e): void => {
+    let pos = document.getElementsByTagName("html")[0].scrollTop;
+    if(pos >= 300 && !this.renderFixedMenu) {
+      this.renderFixedMenu = true;
+    } else if(pos < 100 && this.renderFixedMenu) {
+      this.renderFixedMenu = false;
+    }
+  };
 
 }
