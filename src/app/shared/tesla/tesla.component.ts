21acfb54932c5deb162e588613d2dc492a6e4f0c
Leo Chang-Liao <leo.changliao@gmail.com>
2020-06-06 22:36:59 -0400

    tesla web app - transition update

--- src/app/shared/tesla/tesla.component.ts
+++ src/app/shared/tesla/tesla.component.ts
@@ -7,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeslaComponent implements OnInit {
 
-  public isActive:boolean = false;
+  public appVersion = "beta 2020.0606";
+  public isActive:any = {
+    'trans': false,
+    'show': false
+  };
+  public showApp:boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public start() {
+    this.isActive.trans = true;
+    this.isActive.show = true;
+    setTimeout(() => {
+      this.showApp = true;
+    },200);
+  }
+
+  public end() {    
+    this.showApp = false;
+    this.isActive.trans = false;
+    setTimeout(() => {
+      this.isActive.show = false;
+    },1000);
   }
 
 }
